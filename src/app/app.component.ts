5eeb0c04d3e92ecfb746fc8d5065430ae2b9e8de
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-10-01 22:49:19 +0530

    add router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pixelmate';
+  login!: FormGroup;
+  credential!: any;
+  constructor(private http: HttpClient) {
+    this.login = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
+
+  call() {
+    console.log('s');
+
+    if (this.login.valid) {
+      this.credential = {
+        email: this.login.value['email'],
+        password: this.login.value['password'],
+      };
+      this.login.reset();
+    }
+    try {
+      this.http.get('http://localhost:8080/').subscribe((res) => {
+        console.log(res);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  send() {
+      console.log("crediential set");
+      
+      this.credential = {
+        email: this.login.value['email'],
+        password: this.login.value['password'],
+      };
+      this.login.reset();
+    
+    try {
+      this.http
+        .post('http://localhost:8080/send', this.credential,)
+        .subscribe((res) => {
+          console.log(res);
+          console.log("get daata");
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//angular material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, SignupComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
