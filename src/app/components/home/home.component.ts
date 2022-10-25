b14d7a11bf2f8e3eb7be6efac1fee12933506285
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-10-25 16:05:14 +0530

    project view section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   name: String = 'Prakhar Sahu';
+  index=[1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
+  // index=[1,2,3,4,5,6]
   constructor() {}
 
   ngOnInit(): void {}
