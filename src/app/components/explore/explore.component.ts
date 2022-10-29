0ce77e1d10156086779a03c24b972c92f8c69af6
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-10-29 15:33:37 +0530

    explore page for challenges

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
-  styleUrls: ['./explore.component.css']
+  styleUrls: ['./explore.component.css'],
 })
 export class ExploreComponent implements OnInit {
+  index = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5,
+    6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
