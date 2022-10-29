314881ebdf06242e9a79edd5e9430972bead98f7
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-10-29 14:23:28 +0530

    new component

--- src/app/components/achievements/achievements.component.ts
+++ src/app/components/achievements/achievements.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-achievements',
   templateUrl: './achievements.component.html',
-  styleUrls: ['./achievements.component.css']
+  styleUrls: ['./achievements.component.css'],
 })
 export class AchievementsComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
-  constructor() { }
+  id!: string;
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      // console.log(params);
+      this.id = params['id'];
+      // console.log(this.id); // price
+    });
   }
-
 }
