2de4f278e47ebacb16d9af396ac870fd62f3a567
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-11-01 19:41:06 +0530

    added form fields

--- src/app/components/setting/setting.component.ts
+++ src/app/components/setting/setting.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
-  styleUrls: ['./setting.component.css']
+  styleUrls: ['./setting.component.css'],
 })
 export class SettingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  profile!: FormGroup;
+  user = {
+    name: 'Prakhar sahu',
+    email: 'sahuprakhar022003@gmail.com',
+    username: 'prakhar_5447',
+  };
+  constructor() {
+    this.profile = new FormGroup({
+      name: new FormControl(this.user.name, []),
+      email: new FormControl(this.user.email, [
+        Validators.required,
+        Validators.email,
+      ]),
+      username: new FormControl(this.user.username, [Validators.required]),
+    });
   }
 
+  ngOnInit(): void {}
+
+  save() {
+    console.log('User Details Saved Successfully');
+  }
 }
